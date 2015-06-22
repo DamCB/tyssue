9ffc18231020d60d59305b2078d1ae009e6a5154
Hadrien Mary <hadrien.mary@gmail.com>
2015-06-22 22:25:34 +0200

    Still not perfect but python module can now import C++ lib

--- src/tyssue/core/__init__.py
+++ src/tyssue/core/__init__.py
@@ -1,4 +1,7 @@
-from ..dl_import import dl_import
+import sys
+sys.path.append("/home/hadim/local/conda/envs/tyssue/lib/")
+
+import libtyssue_core as core
 
 from .objects import Epithelium, Vertex, Edge, Face, Volume, Cell, test_import
-dl_import("from ..libtyssue_core import make_hexagon")
+from libtyssue_core import make_hexagon
