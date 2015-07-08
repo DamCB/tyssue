6f0e576dcb6bfacdfeb702d47dc537858f2424dd
Guillaume Gay <guillaume@damcb.com>
2015-07-08 19:04:05 +0200

    WIP - test attribute access (asking travis to test it with CGAL 4.6 while I solve this here)

--- src/cpp-tyssue/objects.cc
+++ src/cpp-tyssue/objects.cc
@@ -53,7 +53,7 @@ void make_polygon(Appical_sheet_3 &sheet, std::vector<Point> &points) {
     Vertex_attribute_handle vh = sheet.create_vertex_attribute(*it);
     sheet.set_vertex_attribute(prev, vh);
     //int id = sheet.info<0>(vh);
-    //std::cout<<"Point: " <<sheet.point_of_vertex_attribute(vh)<<std::endl;
+    std::cout<<"Point: " <<sheet.point_of_vertex_attribute(vh)<<std::endl;
     prev = next;
   };
 };
