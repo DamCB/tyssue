b3e4f65e1e89222c450d2af245e65e440ec23352
Hadrien Mary <hadrien.mary@gmail.com>
2015-07-09 11:35:28 +0200

    Replace binstar to anaconda

--- .full_test.sh
+++ .full_test.sh
@@ -4,7 +4,7 @@ conda create --yes -n $ENV_NAME python=3
 source activate $ENV_NAME
 
 conda install --yes numpy scipy vispy matplotlib nose coverage vispy pip
-conda install --yes -c https://conda.binstar.org/osgeo boost cgal
+conda install --yes -c https://conda.anaconda.org/osgeo boost cgal
 
 mkdir -p build/
 cd build/
