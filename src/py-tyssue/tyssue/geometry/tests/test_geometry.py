4cb376cbf419539623857018834111f351621eef
Guillaume Gay <guillaume@damcb.com>
2015-08-23 17:02:34 +0200

    test wont update , I dont know why

--- src/py-tyssue/tyssue/geometry/tests/test_geometry.py
+++ src/py-tyssue/tyssue/geometry/tests/test_geometry.py
@@ -23,7 +23,7 @@ def test_3cells():
     sgeom.update_normals(sheet)
     norms = np.linalg.norm(eptm.je_df[dcoords],
                            axis=1)
-    np.testing.assert_array_almost_equal(norms, 0.866, rtol=1e-3)
+    np.testing.assert_allclose(norms, 0.866, rtol=1e-3)
     sgeom.update_areas(sheet)
     np.testing.assert_allclose(sheet.cell_df['area'],
                                np.sqrt(3)*1.5, rtol=1e-3)
