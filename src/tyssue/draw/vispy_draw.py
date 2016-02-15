540ab3a8cf37a85e178fb90136cc937ed1633e6b
Guillaume Gay <guillaume@damcb.com>
2016-02-15 19:26:14 +0100

    WIP - tweeking vispy rendering

--- src/tyssue/draw/vispy_draw.py
+++ src/tyssue/draw/vispy_draw.py
@@ -1,4 +1,7 @@
 
+import numpy as np
+import pandas as pd
+
 import vispy as vp
 from vispy import app, scene
 from ..config.json_parser import load_default
@@ -21,17 +24,74 @@ def draw_tyssue(sheet, coords=None, **draw_specs_kw):
     view.bgcolor = vp.color.Color('#aaaaaa')
 
     if draw_specs['face']['visible']:
+        color = None
+        if isinstance(draw_specs['face']['color'], str):
+            face_colors = None
+            color = draw_specs['face']['color']
+
+        colors = np.asarray(draw_specs['face']['color'])
+        if colors.shape == (3,):
+            face_colors = pd.DataFrame(index=sheet.je_df.index,
+                                       columns=['R', 'G', 'B'])
+            for channel, val in zip('RGB', colors):
+                face_colors[channel] = val
+
+        elif colors.shape == (4,):
+            face_colors = pd.DataFrame(index=sheet.je_df.index,
+                                       columns=['R', 'G', 'B', 'A'])
+            for channel, val in zip('RGBA', colors):
+                face_colors[channel] = val
+
+        elif colors.shape in [(sheet.Nf, 3), (sheet.Nf, 4)]:
+            face_colors = pd.DataFrame(index=sheet.face_df.index, data=colors,
+                                       columns=['R', 'G', 'B', 'A'][:colors.shape[1]])
+            face_colors = sheet.upcast_face(face_colors)
+
+        elif colors.shape in [(3, sheet.Ne), (4, sheet.Ne)]:
+            face_colors = pd.DataFrame(index=sheet.face_df.index, data=colors,
+                                       columns=['R', 'G', 'B', 'A'][:colors.shape[1]])
+
         mesh = scene.visuals.Mesh(vertices=vertices,
-                                  faces=faces)
+                                  faces=faces,
+                                  face_colors=face_colors,
+                                  color=color)
         view.add(mesh)
 
     if draw_specs['je']['visible']:
 
+        color = None
+        if isinstance(draw_specs['je']['color'], str):
+            color = draw_specs['je']['color']
+
+        colors = np.asarray(draw_specs['je']['color'])
+        if colors.shape == (3,):
+            color = pd.DataFrame(index=sheet.je_df.index,
+                                 columns=['R', 'G', 'B', 'A'])
+            for channel, val in zip('RGB', colors):
+                color[channel] = val
+            color['A'] = 1.
+
+        elif colors.shape == (4,):
+            color = pd.DataFrame(index=sheet.je_df.index,
+                                 columns=['R', 'G', 'B', 'A'])
+            for channel, val in zip('RGBA', color):
+                color[channel] = val
+
+        elif colors.shape == (3, sheet.Ne):
+            color = pd.DataFrame(index=sheet.je_df.index, data=colors,
+                                 columns=['R', 'G', 'B'])
+            color['A'] = 1.
+
+        elif colors.shape == (4, sheet.Ne):
+            color = pd.DataFrame(index=sheet.je_df.index, data=colors,
+                                 columns=['R', 'G', 'B', 'A'])
+
+
         wire_pos = vertices[sheet.Nc:].copy()
         wire = vp.scene.visuals.Line(pos=wire_pos,
                                      connect=faces[:, :2] - sheet.Nc,
-                                     color=[0.1, 0.1, 0.3, 0.8],
-                                     width=1)
+                                     color=color,
+                                     width=draw_specs['je']['width'])
         view.add(wire)
 
     view.camera.set_range()
