c0ce4cc54c65542e6039318f14a4e204b7eebd06
Guillaume Gay <guillaume@damcb.com>
2016-06-01 13:29:37 +0200

    added multisheet geometry

---
+++ src/tyssue/core/tests/test_multisheet.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+from tyssue.core.multisheet import MultiSheet
+from tyssue.core.generation import hexa_grid2d, from_2d_voronoi
+from scipy.spatial import Voronoi
+
+import tyssue
+
+
+def test_multisheet():
+
+    base_specs = tyssue.config.geometry.sheet_spec()
+    specs = base_specs.copy()
+    specs['face']['layer'] = 0
+    specs['vert']['layer'] = 0
+    specs['vert']['depth'] = 0.0
+    specs['edge']['layer'] = 0
+    specs['settings']['geometry'] = 'flat'
+    layer_args = [(24, 24, 1, 1, 0.4),
+                  (16, 16, 2, 2, 1),
+                  (24, 24, 1, 1, 0.4),
+                  (24, 24, 1, 1, 0.4)]
+    dz = 1.0
+
+    layer_datasets = []
+    for i, args in enumerate(layer_args):
+        centers = hexa_grid2d(*args)
+        data = from_2d_voronoi(Voronoi(centers))
+        data['vert']['z'] = i * dz
+        layer_datasets.append(data)
+
+    msheet = MultiSheet('more',
+                        layer_datasets,
+                        specs)
+    bbox = [[0, 25], [0, 25]]
+    for sheet in msheet:
+        edge_out = sheet.cut_out(bbox, coords=['x', 'y'])
+        sheet.remove(edge_out)
+
+    assert len(msheet) == 4
+    datasets = msheet.concat_datasets()
+    assert np.all(np.isfinite(datasets['vert'][['x', 'y', 'z']]))
+
+    msheet.update_interpolants()
+    for interp in msheet.interpolants:
+        assert np.isfinite(interp(10, 10))
