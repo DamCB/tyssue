9662fd89d437c5711adbf2de77f253df3772ce89
Guillaume Gay <guillaume@damcb.com>
2016-09-20 12:11:57 +0200

    remove translation in load_datasets

--- tyssue/io/hdf5.py
+++ tyssue/io/hdf5.py
@@ -4,23 +4,10 @@ logger = logging.getLogger(name=__name__)
 
 
 def load_datasets(h5store, data_names=['face', 'vert', 'edge']):
-    transl = {'face': 'face',
-              'vert': 'jv',
-              'edge': 'je'}
-    rewrite = False
     with pd.get_store(h5store) as store:
-        try:
-            data = {name: store[name] for name in data_names}
-        except KeyError:
-            logger.warning('Old names file, will be rewritten')
-            data = {name: store[transl[name]] for name in data_names}
-            rewrite = True
-    if rewrite:
-        with pd.get_store(h5store) as store:
-            for key, val in data.items():
-                store.put(key, val)
-
+        data = {name: store[name] for name in data_names}
     return data
+
 
 def save_datasets(h5store, eptm):
     with pd.get_store(h5store) as store:
