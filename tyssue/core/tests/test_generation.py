d34b514fb901876dbb43fdddfecfe2478390b7f6
Bertrand Caré <bertrand.care@gmail.com>
2016-09-22 22:13:21 +0200

    100% coverage for generation.py (3d case for subdivide_faces, extrude with invalid method arg, and anchors tested)

--- tyssue/core/tests/test_generation.py
+++ tyssue/core/tests/test_generation.py
@@ -1,11 +1,16 @@
 from scipy.spatial import Voronoi
 from tyssue.core import generation
 from tyssue.core.sheet import Sheet
-
 from tyssue.core.generation import hexa_grid3d, from_3d_voronoi
 from tyssue.core.generation import hexa_grid2d, from_2d_voronoi
 from tyssue.core.generation import extrude, subdivide_faces
 
+from tyssue.core.objects import get_opposite
+
+from pytest import raises
+
+import numpy as np
+import pandas as pd
 
 def test_3faces():
 
@@ -23,6 +28,8 @@ def test_from_3d_voronoi():
     assert datasets['edge'].shape[0] == 1272
     assert datasets['face'].shape[0] == 283
     assert datasets['cell'].shape[0] == 72
+
+    
 
 
 def test_from_2d_voronoi():
@@ -53,3 +60,48 @@ def test_subdivide():
     assert subdivided['face'].shape[0] == 3
     assert subdivided['edge'].shape[0] == 30
     assert subdivided['vert'].shape[0] == 14
+
+    datasets_3d = extrude(datasets, method="translation")
+    sheet_3d = Sheet('test3d', datasets_3d, specs)
+    subdivided_3d = subdivide_faces(sheet_3d, [0, ])
+    assert subdivided_3d['face'].shape[0] == 24
+    assert subdivided_3d['edge'].shape[0] == 120
+    assert subdivided_3d['vert'].shape[0] == 27
+    assert subdivided_3d['cell'].shape[0] == 3
+
+
+def test_extrude_invalid_method():
+    datasets, specs = generation.three_faces_sheet()
+    with raises(ValueError, msg='Expecting a ValueError : method should be \'translation\' or \'homothecy\''):
+        datasets_3d = extrude(datasets, method='invalid_method')
+
+
+def test_hexagrid3d_noise():
+    np.random.seed(1)
+    grid = hexa_grid3d(6, 4, 3, noise = 0.1)
+    datasets = from_3d_voronoi(Voronoi(grid))
+    assert datasets['vert'].shape[0] == 318
+    assert datasets['edge'].shape[0] == 3300
+    assert datasets['face'].shape[0] == 409
+    assert datasets['cell'].shape[0] == 72
+
+
+def test_anchors():
+    datasets, specs = generation.three_faces_sheet()
+    sheet = Sheet('test_anchors', datasets, specs)
+
+    sheet.edge_df['opposite'] = get_opposite(sheet.edge_df)
+    
+    expected_dict = {18:[1,13],19:[2,14],20:[3,15],21:[4,16],22:[5,17],23:[6,18],24:[7,19],25:[8,20],26:[9,21],27:[10,22],28:[11,23],29:[12,24]}
+
+    
+    expected_res = pd.DataFrame.from_dict(expected_dict, orient="index")
+    expected_res.columns = ['srce','trgt']
+    
+    
+    generation.create_anchors(sheet)
+
+    res_srce_trgt_anchors = sheet.edge_df.loc[18:, ['srce','trgt']]
+    
+    assert res_srce_trgt_anchors.equals(expected_res)
+    
