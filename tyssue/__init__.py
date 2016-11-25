c647dc9f18cc50eb716401fb9d0002402e8cb4df
Guillaume Gay <guillaume@damcb.com>
2016-11-25 12:27:10 +0100

    adds import short cuts for various classes

--- tyssue/__init__.py
+++ tyssue/__init__.py
@@ -1,2 +1,8 @@
 from .core import objects
 from .version import version as __version__
+from .core.objects import Epithelium
+from .core.sheet import Sheet
+from .core.monolayer import Monolayer, MonolayerWithLamina
+from .geometry.planar_geometry import PlanarGeometry
+from .geometry.sheet_geometry import SheetGeometry
+from .geometry.bulk_geometry import BulkGeometry, MonoLayerGeometry
--- tyssue/dynamics/__init__.py
+++ tyssue/dynamics/__init__.py
@@ -1,1 +1,4 @@
 '''dynamics'''
+from .sheet_vertex_model import SheetModel
+from .planar_vertex_model import PlanarModel
+from .bulk_model import BulkModel, LaminaModel
