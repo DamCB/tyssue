532cba988ecd0347d304997b39bcf4826aeb6d82
Guillaume Gay <guillaume@damcb.com>
2016-11-28 18:57:54 +0100

    shorter import statements

--- tyssue/__init__.py
+++ tyssue/__init__.py
@@ -3,6 +3,8 @@ from .version import version as __version__
 from .core.objects import Epithelium
 from .core.sheet import Sheet
 from .core.monolayer import Monolayer, MonolayerWithLamina
+from .core.multisheet import MultiSheet
 from .geometry.planar_geometry import PlanarGeometry
 from .geometry.sheet_geometry import SheetGeometry
 from .geometry.bulk_geometry import BulkGeometry, MonoLayerGeometry
+from .geometry.multisheetgeometry import MultiSheetGeometry
--- tyssue/dynamics/__init__.py
+++ tyssue/dynamics/__init__.py
@@ -2,3 +2,4 @@
 from .sheet_vertex_model import SheetModel
 from .planar_vertex_model import PlanarModel
 from .bulk_model import BulkModel, LaminaModel
+from .multisheetmodel import MultiSheetModel
