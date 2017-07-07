992dd8b4b775d2a546966788672227ef6e887192
Guillaume Gay <guillaume@damcb.com>
2017-07-07 09:32:04 +0200

    replacing deprecated pandas `get_store` by HDFStore

--- tyssue/io/hdf5.py
+++ tyssue/io/hdf5.py
@@ -4,12 +4,12 @@ logger = logging.getLogger(name=__name__)
 
 
 def load_datasets(h5store, data_names=['face', 'vert', 'edge']):
-    with pd.get_store(h5store) as store:
+    with pd.HDFStore(h5store) as store:
         data = {name: store[name] for name in data_names}
     return data
 
 
 def save_datasets(h5store, eptm):
-    with pd.get_store(h5store) as store:
+    with pd.HDFStore(h5store) as store:
         for key in eptm.data_names:
             store.put(key, getattr(eptm, '{}_df'.format(key)))
