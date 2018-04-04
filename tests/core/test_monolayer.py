3a7cbed3d6abf280c0addea75d4cd210ad881134
Guillaume Gay <guillaume@damcb.com>
2018-04-04 19:20:44 +0200

    removed deprecated files and increased test coverage

--- tests/core/test_monolayer.py
+++ tests/core/test_monolayer.py
@@ -1,5 +1,33 @@
+from numpy.testing import assert_array_equal
+import numpy as np
+
 from tyssue.generation import extrude, three_faces_sheet
 from tyssue import Monolayer, config
+
+def test_monolayer():
+    datasets, specs = three_faces_sheet()
+    extruded = extrude(datasets, method='translation')
+    mono = Monolayer('test', extruded,
+                     config.geometry.bulk_spec())
+
+    assert_array_equal(mono.apical_verts.values,
+                       np.arange(13))
+    assert_array_equal(mono.basal_verts.values,
+                       np.arange(13)+13)
+
+    assert_array_equal(mono.apical_edges.values,
+                       np.arange(18))
+    assert_array_equal(mono.basal_edges.values,
+                       np.arange(18)+18)
+    assert_array_equal(mono.sagittal_edges.values,
+                       np.arange(72)+36)
+
+    assert_array_equal(mono.apical_faces.values,
+                       np.arange(3))
+    assert_array_equal(mono.basal_faces.values,
+                       np.arange(3)+3)
+    assert_array_equal(mono.sagittal_faces.values,
+                       np.arange(18)+6)
 
 
 def test_copy():
@@ -16,3 +44,4 @@ def test_copy():
     mono2 = mono.copy()
 
     assert mono2.Nc == 3
+    assert isinstance(mono2, Monolayer)
--- tyssue/core/generation.py
+++
@@ -1,4 +0,0 @@
-import warnings
-warnings.warn('generation should not be imported from core anymore but from its own module')
-
-#from ..generation import *
