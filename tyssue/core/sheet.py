db529222c12d9406a60889eef2da101d7d3a0cb4
Guillaume Gay <guillaume@damcb.com>
2018-09-07 16:54:21 +0200

    reset catched ValueError to Exception waiting for pandas to publish 0.24

--- tyssue/core/sheet.py
+++ tyssue/core/sheet.py
@@ -364,7 +364,9 @@ def get_opposite(edge_df):
     flipped.names = ['srce', 'trgt']
     try:
         opposite = st_indexed.reindex(flipped)['edge'].values
-    except ValueError:
+    #except ValueError: # see https://github.com/pandas-dev/pandas/issues/21770, will be fixed in
+                        # pandas 0.24
+    except Exception :
         dup = flipped.duplicated()
         warnings.warn('Duplicated (`srce`, `trgt`) values in edge_df, maybe sanitize your input')
         opposite = st_indexed[~dup].reindex(flipped)['edge'].values
