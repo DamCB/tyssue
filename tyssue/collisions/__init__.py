9ed967a727820a392b68cdf0dba4d4c6fb0f3782
Guillaume Gay <guillaume@damcb.com>
2018-11-16 12:13:07 +0100

    WIP integrating collisions in tyssue

---
+++ tyssue/collisions/__init__.py
@@ -0,0 +1,10 @@
+import numpy as np
+import c_collisions
+
+
+def self_intersections(sheet):
+    faces, vertices = sheet.triangular_mesh(sheet.coords, return_mask=False)
+    mesh = c_collisions.sheet_to_surface_mesh(faces, vertices)
+    if not c_collisions.does_self_intersect(mesh):
+        return np.empty((0, 2), dtype=np.int)
+    return np.array(c_collisions.self_intersections(mesh), dtype=np.int)
