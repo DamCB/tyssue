f2cd8bb237132262f8aad952015c8944fdd7c5d0
Sophie THEIS <sophiets13@gmail.com>
2018-11-30 09:50:21 +0100

    Add list of except event that can be add several time for the same cell
    Improve test for the event manager class.

--- tests/behaviors/test_sheet_events.py
+++ tests/behaviors/test_sheet_events.py
@@ -12,7 +12,8 @@ from tyssue.geometry.sheet_geometry import SheetGeometry as geom
 
 from tyssue.behaviors.event_manager import EventManager, wait
 from tyssue.behaviors.sheet.basic_events import (
-    division, contraction, type1_transition, face_elimination, check_tri_faces)
+    division, contraction, contraction_neighbor,
+    type1_transition, face_elimination, check_tri_faces)
 from tyssue.behaviors.sheet.actions import (
     contract, ab_pull, relax, increase_linear_tension, grow, shrink)
 from tyssue.behaviors.sheet.actions import remove as type3
@@ -42,6 +43,20 @@ def test_add_only_once():
     manager.execute(None)
     manager.update()
     assert len(manager.current) == 2
+
+
+def test_add_several_time_same_events():
+    manager = EventManager('face')
+    initial_cell_event = [(division, {'face_id': 1, 'geom': geom}),
+                          (contraction_neighbor, {'face_id': 2}),
+                          (contraction_neighbor, {'face_id': 3}),
+                          (apoptosis, {'face_id': 3, 'shrink_rate': 4}),
+                          (contraction_neighbor, {'face_id': 2})]
+
+    manager.extend(initial_cell_event)
+    manager.execute(None)
+    manager.update()
+    assert len(manager.current) == 5
 
 
 def test_logging():
@@ -148,6 +163,22 @@ def test_execute_contraction():
     manager.update()
     assert sheet.face_df.loc[face_id, 'contractility'] == 1.32
 
+
+def test_execute_contraction_neighbor():
+    sheet = Sheet('emin', *three_faces_sheet())
+    geom.update_all(sheet)
+    sheet.face_df['contractility'] = 1.12
+
+    sheet.face_df['id'] = sheet.face_df.index.values
+    manager = EventManager('face')
+    face_id = 1
+    event = (contraction_neighbor, {'face_id': face_id,
+                                    'contractile_increase': 0.2,
+                                    'critical_area': 1e-2})
+    manager.current.append(event)
+    manager.execute(sheet)
+    manager.update()
+    assert sheet.face_df.loc[face_id, 'contractility'] == 1.32
 
 
 def test_type1_transition():
