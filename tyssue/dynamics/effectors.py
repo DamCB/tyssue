77804f0ed5017b92aae78c3444c0eefaf2d4b652
George C <58273742+gcourcou@users.noreply.github.com>
2020-02-25 17:48:41 -0800

    Update effectors.py

--- tyssue/dynamics/effectors.py
+++ tyssue/dynamics/effectors.py
@@ -457,8 +457,10 @@ class SurfaceTension(AbstractEffector):
     def gradient(eptm):
 
         G = to_nd(eptm.upcast_face(eptm.face_df["surface_tension"]), len(eptm.coords))
-        grad_a_srce, grad_a_trgt = area_grad(eptm)
-
+        try:
+            grad_a_srce, grad_a_trgt = area_grad(eptm)
+        except:
+            grad_a_srce, grad_a_trgt = lumen_area_grad(eptm)
         grad_a_srce = G * grad_a_srce
         grad_a_trgt = G * grad_a_trgt
         grad_a_srce.columns = ["g" + u for u in eptm.coords]
