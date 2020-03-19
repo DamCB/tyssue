7571518260a7c21e4629b7a842663bbec6d3a66a
Guillaume Gay <guillaume@damcb.com>
2020-03-19 17:01:28 +0100

    fixed a bug with string lengths in the segment columns

--- tests/core/test_history.py
+++ tests/core/test_history.py
@@ -20,21 +20,16 @@ def test_simple_history():
     assert "dx" in history.datasets["edge"].columns
 
     for element in sheet.datasets:
-        assert sheet.datasets[element].shape[
-            0] == history.datasets[element].shape[0]
+        assert sheet.datasets[element].shape[0] == history.datasets[element].shape[0]
     history.record()
-    assert sheet.datasets["vert"].shape[0] * \
-        2 == history.datasets["vert"].shape[0]
+    assert sheet.datasets["vert"].shape[0] * 2 == history.datasets["vert"].shape[0]
     history.record()
-    assert sheet.datasets["vert"].shape[0] * \
-        3 == history.datasets["vert"].shape[0]
-    assert sheet.datasets["face"].shape[0] * \
-        3 == history.datasets["face"].shape[0]
+    assert sheet.datasets["vert"].shape[0] * 3 == history.datasets["vert"].shape[0]
+    assert sheet.datasets["face"].shape[0] * 3 == history.datasets["face"].shape[0]
     mono = Epithelium("eptm", extrude(sheet.datasets))
     histo2 = History(mono)
     for element in mono.datasets:
-        assert mono.datasets[element].shape[
-            0] == histo2.datasets[element].shape[0]
+        assert mono.datasets[element].shape[0] == histo2.datasets[element].shape[0]
 
 
 def test_warning():
@@ -98,8 +93,7 @@ def test_historyHDF5_path_warning():
         history.record(time_stamp=0)
 
     with pytest.warns(UserWarning):
-        history = HistoryHdf5(
-            sheet, extra_cols={"edge": ["dx"]}, hf5file="out.hf5")
+        history = HistoryHdf5(sheet, extra_cols={"edge": ["dx"]}, hf5file="out.hf5")
         history.record(time_stamp=0)
 
     os.remove("out.hf5")
@@ -111,8 +105,7 @@ def test_historyHDF5_retrieve():
     history = HistoryHdf5(sheet, extra_cols={"edge": ["dx"]})
 
     for element in sheet.datasets:
-        assert sheet.datasets[element].shape[
-            0] == history.datasets[element].shape[0]
+        assert sheet.datasets[element].shape[0] == history.datasets[element].shape[0]
     history.record(time_stamp=0)
     history.record(time_stamp=1)
     sheet_ = history.retrieve(0)
@@ -129,12 +122,10 @@ def test_historyHDF5_retrieve():
 
 def test_historyHDF5_save_every():
     sheet = Sheet("3", *three_faces_sheet())
-    history = HistoryHdf5(
-        sheet, extra_cols={"edge": ["dx"]}, save_every=2, dt=1)
+    history = HistoryHdf5(sheet, extra_cols={"edge": ["dx"]}, save_every=2, dt=1)
 
     for element in sheet.datasets:
-        assert sheet.datasets[element].shape[
-            0] == history.datasets[element].shape[0]
+        assert sheet.datasets[element].shape[0] == history.datasets[element].shape[0]
     for i in range(6):
         history.record(time_stamp=i)
     sheet_ = history.retrieve(0)
@@ -155,24 +146,52 @@ def test_historyHDF5_save_every():
     os.remove("out.hf5")
 
 
-def test_historyHDF5_save_other_sheet():
+def test_historyHDF5_itemsize():
     sheet = Sheet("3", *three_faces_sheet())
+    sheet.vert_df["segment"] = "apical"
     history = HistoryHdf5(
-        sheet, extra_cols={"edge": ["dx"], "face": ['area']})
+        sheet, extra_cols={"edge": ["dx"], "face": ["area"], "vert": ["segment"]}
+    )
 
     for element in sheet.datasets:
-        assert sheet.datasets[element].shape[
-            0] == history.datasets[element].shape[0]
-    sheet.face_df.loc[0, 'area'] = 1.
+        assert sheet.datasets[element].shape[0] == history.datasets[element].shape[0]
+    sheet.vert_df.loc[0, "segment"] = ""
     history.record(time_stamp=1)
 
-    sheet.face_df.loc[0, 'area'] = 12.
+    sheet.vert_df.loc[0, "segment"] = "lateral"
+    history.record(time_stamp=2)
+
+    sheet.face_df.loc[0, "area"] = 12.0
+    history.record(time_stamp=3, sheet=sheet)
+
+    sheet1_ = history.retrieve(1)
+    assert sheet1_.vert_df.loc[0, "segment"] == ""
+
+    sheet2_ = history.retrieve(2)
+    assert sheet2_.vert_df.loc[0, "segment"] == "lateral"
+
+    sheet3_ = history.retrieve(3)
+    assert sheet3_.face_df.loc[0, "area"] == 12.0
+
+    os.remove("out.hf5")
+
+
+def test_historyHDF5_save_other_sheet():
+    sheet = Sheet("3", *three_faces_sheet())
+    history = HistoryHdf5(sheet, extra_cols={"edge": ["dx"], "face": ["area"]})
+
+    for element in sheet.datasets:
+        assert sheet.datasets[element].shape[0] == history.datasets[element].shape[0]
+    sheet.face_df.loc[0, "area"] = 1.0
+    history.record(time_stamp=1)
+
+    sheet.face_df.loc[0, "area"] = 12.0
     history.record(time_stamp=2, sheet=sheet)
 
     sheet1_ = history.retrieve(1)
-    assert sheet1_.face_df.loc[0, 'area'] == 1.
+    assert sheet1_.face_df.loc[0, "area"] == 1.0
     sheet2_ = history.retrieve(2)
-    assert sheet2_.face_df.loc[0, 'area'] == 12.
+    assert sheet2_.face_df.loc[0, "area"] == 12.0
 
     os.remove("out.hf5")
 
@@ -210,14 +229,18 @@ def test_to_and_from_archive():
 
 def test_unsaved_col():
     sheet = Sheet("3", *three_faces_sheet())
-    history = HistoryHdf5(sheet,
-                          extra_cols={"face": sheet.face_df.columns,
-                                      "edge": list(sheet.edge_df.columns),
-                                      "vert": list(sheet.vert_df.columns)},
-                          hf5file="test.hf5")
+    history = HistoryHdf5(
+        sheet,
+        extra_cols={
+            "face": sheet.face_df.columns,
+            "edge": list(sheet.edge_df.columns),
+            "vert": list(sheet.vert_df.columns),
+        },
+        hf5file="test.hf5",
+    )
     history.record()
     history.record()
-    sheet.face_df['new_col'] = 0
+    sheet.face_df["new_col"] = 0
     with pytest.warns(UserWarning):
         history.record()
     os.remove("test.hf5")
@@ -225,15 +248,18 @@ def test_unsaved_col():
 
 def test_change_col_types():
     sheet = Sheet("3", *three_faces_sheet())
-    history = HistoryHdf5(sheet,
-                          extra_cols={"face": sheet.face_df.columns,
-                                      "edge": list(
-                                          sheet.edge_df.columns),
-                                      "vert": list(sheet.vert_df.columns)},
-                          hf5file="test.hf5")
+    history = HistoryHdf5(
+        sheet,
+        extra_cols={
+            "face": sheet.face_df.columns,
+            "edge": list(sheet.edge_df.columns),
+            "vert": list(sheet.vert_df.columns),
+        },
+        hf5file="test.hf5",
+    )
     history.record()
     history.record()
-    sheet.face_df['z'] = 'abc'
+    sheet.face_df["z"] = "abc"
     with pytest.raises(ValueError):
         history.record()
     os.remove("test.hf5")
--- tyssue/core/history.py
+++ tyssue/core/history.py
@@ -105,8 +105,14 @@ class History:
 
         """
         with pd.HDFStore(hf5file, "a") as store:
+
             for key, df in self.datasets.items():
-                store.append(key=key, value=df, data_columns=["time"])
+                kwargs = {"data_columns": ["time"]}
+                if "segment" in df.columns:
+                    kwargs["min_itemsize"] = {
+                        "segment": 7,
+                    }
+                store.append(key=key, value=df, **kwargs)
 
     @property
     def time_stamps(self):
@@ -259,7 +265,7 @@ class HistoryHdf5(History):
             last = self.time_stamps[-1]
             with pd.HDFStore(self.hf5file, "r") as file:
                 keys = file.keys()
-            if "\cell" in keys:
+            if r"\cell" in keys:
                 sheet = Epithelium
 
         History.__init__(self, sheet, save_every, dt, extra_cols)
@@ -338,11 +344,14 @@ class HistoryHdf5(History):
                 new_types = dtypes_[element].to_dict()
                 if new_types != old_types:
                     changed_type = {
-                        k: old_types[k] for k in old_types
-                        if k in new_types and old_types[k] != new_types[k]}
+                        k: old_types[k]
+                        for k in old_types
+                        if k in new_types and old_types[k] != new_types[k]
+                    }
                     raise ValueError(
                         "There is a change of datatype in {} table in {} columns".format(
-                            element, changed_type)
+                            element, changed_type
+                        )
                     )
 
         if (self.save_every is None) or (
@@ -352,9 +361,13 @@ class HistoryHdf5(History):
                 times = pd.Series(np.ones((df.shape[0],)) * self.time, name="time")
                 df = df[self.columns[element]]
                 df = pd.concat([df, times], ignore_index=False, axis=1, sort=False)
-
+                kwargs = {"data_columns": ["time"]}
+                if "segment" in df.columns:
+                    kwargs["min_itemsize"] = {
+                        "segment": 8,
+                    }
                 with pd.HDFStore(self.hf5file, "a") as file:
-                    file.append(key=element, value=df, data_columns=["time"])
+                    file.append(key=element, value=df, **kwargs)
 
         self.index += 1
 
