85a5fe0648fb76d0d200e8bc37546ef45938ca29
Guillaume Gay <guillaume@damcb.com>
2021-05-12 14:53:20 +0200

    cleans dtype, fixing deprecation warning from numpy

--- tyssue/collisions/intersection.py
+++ tyssue/collisions/intersection.py
@@ -28,5 +28,5 @@ def self_intersections(sheet):
     faces, vertices = sheet.triangular_mesh(sheet.coords, return_mask=False)
     mesh = c_collisions.sheet_to_surface_mesh(faces, vertices)
     if not c_collisions.does_self_intersect(mesh):
-        return np.empty((0, 2), dtype=np.int)
-    return np.array(c_collisions.self_intersections(mesh), dtype=np.int)
+        return np.empty((0, 2), dtype=int)
+    return np.array(c_collisions.self_intersections(mesh), dtype=int)
