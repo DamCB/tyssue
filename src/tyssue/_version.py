63aea945b9977727991711e6fcfe2dacdb4087c5
Guillaume Gay <guillaume@damcb.com>
2023-12-28 23:23:50 +0100

    simplified I guess

--- src/tyssue/_version.py
+++ src/tyssue/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.9.1.dev113+gcbfd1f8.d20231228'
-__version_tuple__ = version_tuple = (0, 9, 1, 'dev113', 'gcbfd1f8.d20231228')
+__version__ = version = '0.9.1.dev115+g76daa2a.d20231228'
+__version_tuple__ = version_tuple = (0, 9, 1, 'dev115', 'g76daa2a.d20231228')
