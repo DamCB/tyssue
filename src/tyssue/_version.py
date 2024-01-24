2b3a5728fa2411db2764c00719a6621479491652
Guillaume Gay <guillaume@damcb.com>
2024-01-24 15:05:11 +0100

    tagging again to avoid metadata mismatch

--- src/tyssue/_version.py
+++ src/tyssue/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.9.1.dev125+gb96792f.d20240109'
-__version_tuple__ = version_tuple = (0, 9, 1, 'dev125', 'gb96792f.d20240109')
+__version__ = version = '1.0rc2.dev0+gef71914.d20240124'
+__version_tuple__ = version_tuple = (1, 0, 'dev0', 'gef71914.d20240124')
